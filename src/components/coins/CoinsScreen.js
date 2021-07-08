63ac845e05219be4c3e4b5bfdffb6b9720fb3874
Adrigondo <18040238@cobachih.edu.mx>
2021-07-08 13:29:48 -0600

    CoinsStack and CoinsScreen components created

---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+class CoinsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Coins Screen
+        </Text>
+      </View>
+    );
+  }
+}
+export default CoinsScreen;
